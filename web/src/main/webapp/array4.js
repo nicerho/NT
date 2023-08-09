eb22e5fffedd9378c93eb4c1cb1f2395faaa6cb5
nicerho <131422003+nicerho@users.noreply.github.com>
2023-08-09 18:00:09 +0900

    230809

--- web/src/main/webapp/array4.js
+++ web/src/main/webapp/array4.js
@@ -8,7 +8,7 @@ function idck() {
 let apost;
 let bdata;
 function ajax(data) {
-	var ajax_data = "mid=" + data;
+	var ajax_data = "mid=" + data; //Back에게 전달형태 및 데이터값 
 	function aaa() {
 		if (window.XMLHttpRequest) {
 			return new XMLHttpRequest();
@@ -18,14 +18,19 @@ function ajax(data) {
 		if (apost.readyState == XMLHttpRequest.DONE && apost.status == 200) {
 			bdata = this.response;
 			return_resp(bdata) // 리턴 메세지를 별도로 핸들링 하는 함수로 전달
-
 		}
 	}
 	apost = aaa();
 	apost.onreadystatechange = bbb;
 	apost.open("POST", "http://192.168.110.223:8081/web/login_idck.jsp", true);
+	//POST전송에만 사용되는 enctype 구조이며, 사용자가 입력한 모든 데이터를 암호화로 사용한다는 뜻
 	apost.setRequestHeader("content-type", "application/x-www-form-urlencoded");
 	apost.send(ajax_data);
+	/**
+	 * GET 전송일 경우
+	 * apost.open("POST", "http://192.168.110.223:8081/web/login_idck.jsp?mid="+data, true);
+	 * apost.send()
+	 */
 }
 function return_resp(rp) {
 	console.log(rp);
@@ -36,6 +41,6 @@ function return_resp(rp) {
 	}
 	else {
 		alert("OK")
+		document.getElementById("mid").readOnly = true;
 	}
-
 }
--- web/src/main/webapp/login_idck.jsp
+++ web/src/main/webapp/login_idck.jsp
@@ -1,11 +1,17 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8" trimDirectiveWhitespaces="true"%>
+
+<!--  trimDirectiveWhitespaces=false (out.print 적용시 공백 적용)
+trimDirectiveWhitespaces=true JSP문법이 공백없이 결과값만 적용-->
+
 <%
 /*response.addHeader : CORS(도메인 다른 경우 또는 외부서버에서 접속을 할 때 보안상 접속을 자동차단) - 접속허가를 해주는 클래스*/
 response.addHeader("Access-Control-Allow-Origin", "*");
 response.addHeader("Access-Control-Allow-Credentials", "true");
+/*intern() : 문자열을 연산기호로 조건문에 사용할 수 있도록 하는 클래스*/
 String userid = request.getParameter("mid").intern();
 String msg = "";
+//Database : select로 row 확인작업
 if (userid == "admin") {
 	msg = "no";
 } else if (userid == "") {
@@ -13,5 +19,5 @@ if (userid == "admin") {
 } else {
 	msg = "yes";
 }
-out.print(msg);
+out.print(msg); //ajax로 요청한 사항을 Front로 return
 %>
